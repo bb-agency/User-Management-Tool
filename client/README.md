3ec92e8ed9d0565531bab52d4f9f63433c224188
ivan-krizaj <ivan.krizaj@bb.agency>
2022-08-31 13:54:59 +0200

    initial commit

---
+++ client/README.md
@@ -0,0 +1,1 @@
+User Managment Tool template project
---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+const moongose = require("mongoose");
+
+const userSchema = moongose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = moongose.model("User", userSchema);
